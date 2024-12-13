0595968648ab2f79bde345bee00114bd2fd67f7c
bkataru <baalateja.k@gmail.com>
2024-12-13 22:56:53 +0530

    completed

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # port-scanner-in-async-rust
 
 following [Make a port scanner in #rustlang with Tokio and learn async Rust 🦀 Learn Rust Creatively](https://www.youtube.com/watch?v=J3C6sNK2wnk)
+
+## note
+
+in `porter`'s original implementation, the port scan does not work when a single IP address is specified:
+
+```bash
+$ porter 127.0.0.1
+```
+
+the program errors out with:
+
+![alt text](./docs/image.png)
--- porter/src/main.rs
+++ porter/src/main.rs
@@ -1,3 +1,87 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::net::IpAddr;
+
+use tokio::{
+    net::TcpStream,
+    runtime::Runtime,
+    sync::mpsc::{self},
+};
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// IP address of the port scan.
+    #[arg(conflicts_with("cidr"), required_unless_present("cidr"))]
+    addr: Option<IpAddr>,
+
+    #[arg(long)]
+    cidr: Option<cidr::IpCidr>,
+
+    /// Start of the range.
+    #[arg(short = 's', long, default_value_t = 1)]
+    port_start: u16,
+
+    // End of the range of ports to scan (inclusive).
+    #[arg(short = 'e', long, default_value_t = 1024)]
+    port_end: u16,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    assert!(args.port_start != 0);
+    assert!(args.port_start <= args.port_end);
+
+    let rt = Runtime::new()?;
+    let (tx, mut rx) = mpsc::channel(10);
+
+    // let mut open_ports = vec![];
+    rt.block_on(async {
+        let (mut from_single, mut from_cidr);
+
+        let addresses: &mut dyn Iterator<Item = IpAddr>;
+
+        match (args.addr, args.cidr) {
+            (Some(addr), _) => {
+                from_single = vec![addr].into_iter();
+                addresses = &mut from_single;
+            }
+            (_, Some(cidr)) => {
+                from_cidr = cidr.iter().map(|net| net.address());
+                addresses = &mut from_cidr;
+            }
+            (_, _) => unreachable!(),
+        }
+
+        for addr in addresses {
+            println!("? {addr}:{}-{}", args.port_start, args.port_end);
+            for port in args.port_start..=args.port_end {
+                let tx = tx.clone();
+                tokio::spawn(async move {
+                    if let Err(err) = scan(addr, port, tx).await {
+                        eprintln!("error: {err}");
+                    };
+                });
+            }
+        }
+    });
+
+    drop(tx);
+
+    while let Ok((addr, port)) = rx.try_recv() {
+        println!("= {addr}:{port}");
+    }
+
+    Ok(())
+}
+
+async fn scan(
+    addr: IpAddr,
+    port: u16,
+    results_tx: mpsc::Sender<(IpAddr, u16)>,
+) -> Result<(), mpsc::error::SendError<(IpAddr, u16)>> {
+    if let Ok(_ping) = TcpStream::connect((addr, port)).await {
+        results_tx.send((addr, port)).await?;
+        //&mut open_ports.push((args.addr, port));
+    }
+
+    Ok(())
 }
